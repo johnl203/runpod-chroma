a11db73eff9585aab56aa22137fb5a0257dd531f
johnl203 <johnlukas@live.com>
2025-07-02 16:13:54 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-cd /ComfyUI/models/diffusion_models/
+cd /workspace/ComfyUI/models/diffusion_models/
 wget "https://huggingface.co/lodestones/Chroma/resolve/main/chroma-unlocked-v41-detail-calibrated.safetensors?download=true" -O "chroma-unlocked-v41-detail-calibrated.safetensors"
 
-cd /ComfyUI/models/vae/
+cd /workspace//ComfyUI/models/vae/
 wget "https://huggingface.co/lodestones/Chroma/resolve/main/vae/diffusion_pytorch_model.safetensors?download=true" -O "ae.safetensors"
 
-cd /ComfyUI/models/text_encoders
+cd /workspace//ComfyUI/models/text_encoders
 wget "https://huggingface.co/easygoing0114/flan-t5-xxl-fused/resolve/main/flan_t5_xxl_TE-only_FP16.safetensors?download=true" -O "flan_t5_xxl_TE-only_FP16.safetensors"
 
-cd /ComfyUI/user/default/workflows/
+cd /workspace/ComfyUI/user/default/workflows/
 wget "https://huggingface.co/lodestones/Chroma/resolve/main/ChromaSimpleWorkflow20250507.json"
